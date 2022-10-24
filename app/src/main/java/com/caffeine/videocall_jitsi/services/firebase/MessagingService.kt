fc05cd6ff8c0960198ab9bef1b96283b54a2994d
Saad <saadahmedscse@gmail.com>
2022-10-24 15:51:40 +0600

    Call Accept, Reject, Hang, Ringing Completed

--- app/src/main/java/com/caffeine/videocall_jitsi/services/firebase/MessagingService.kt
+++ app/src/main/java/com/caffeine/videocall_jitsi/services/firebase/MessagingService.kt
@@ -1,7 +1,7 @@
 package com.caffeine.videocall_jitsi.services.firebase
 
 import android.content.Intent
-import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.caffeine.videocall_jitsi.utils.SessionManager
 import com.caffeine.videocall_jitsi.view.calling.IncomingCallActivity
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -16,18 +16,37 @@ class MessagingService : FirebaseMessagingService() {
         super.onMessageReceived(message)
 
         val data = message.data
-        val uid = data["uid"]
-        val senderName = data["senderName"]
-        val senderEmail = data["senderEmail"]
-        val senderUid = data["senderUid"]
 
-        if (uid == SessionManager.getInstance(applicationContext).uid) {
-            val i = Intent(applicationContext, IncomingCallActivity::class.java)
-            i.putExtra("uid", senderUid)
-            i.putExtra("email", senderEmail)
-            i.putExtra("name", senderName)
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(i)
+        when (data["status"]) {
+            "ringing" -> {
+                val uid = data["uid"]
+                val senderName = data["senderName"]
+                val senderEmail = data["senderEmail"]
+                val senderUid = data["senderUid"]
+                if (uid == SessionManager.getInstance(applicationContext).uid) {
+                    val i = Intent(applicationContext, IncomingCallActivity::class.java)
+                    i.putExtra("uid", senderUid)
+                    i.putExtra("email", senderEmail)
+                    i.putExtra("name", senderName)
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(i)
+                }
+            }
+            "accepted" -> {
+                val i = Intent("ON_CALL_ACTION")
+                i.putExtra("call_status", "accepted")
+                LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(i)
+            }
+            "rejected" -> {
+                val i = Intent("ON_CALL_ACTION")
+                i.putExtra("call_status", "rejected")
+                LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(i)
+            }
+            "hangup" -> {
+                val i = Intent("ON_CALL_ACTION")
+                i.putExtra("call_status", "hangup")
+                LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(i)
+            }
         }
     }
 }
